d298beb02687aa5b201a3f5114cb783ee8d1680f
JrzenonDev <jrobertoonb@gmail.com>
2024-10-02 22:08:45 -0300

    (feat)[feature/main] adding post routes and create middleware to parsing json

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import postRoutes from "./routes/postRoutes";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware parsing  JSON
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL || "mongodb://mongo:27017/meubanco")
   .then(() => console.log("MongoDB conectado com sucesso"))
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(postRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
