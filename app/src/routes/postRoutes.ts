baf9151060eb6740a4b685993301a42fae1e9870
JrzenonDev <jrobertoonb@gmail.com>
2024-10-06 10:47:31 -0300

    (feat)[feature/main] create route dele and update post

--- app/src/routes/postRoutes.ts
+++ app/src/routes/postRoutes.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.get("/posts", PostController.getAllPosts);
 router.get("/posts/:id", PostController.getPostById);
 router.post("/posts", PostController.createPost);
+router.put("/posts/:id", PostController.updatePost);
+router.delete("/posts/:id", PostController.deletePost);
 
 export default router;
