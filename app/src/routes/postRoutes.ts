8d777a36973449bc315617d60b243c6530e647bf
JrzenonDev <jrobertoonb@gmail.com>
2024-10-11 14:24:57 -0300

    (feat)[feature/main] config swagger in routes

--- app/src/routes/postRoutes.ts
+++ app/src/routes/postRoutes.ts
@@ -3,10 +3,102 @@ import { PostController } from "../controllers/postController";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Retorna todos os posts
+ *     responses:
+ *       200:
+ *         description: Lista de posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.get("/posts", PostController.getAllPosts);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Retorna um post pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do post
+ *     responses:
+ *       200:
+ *         description: Post retornado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ */
 router.get("/posts/:id", PostController.getPostById);
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Cria um novo post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       201:
+ *         description: Post criado com sucesso
+ */
 router.post("/posts", PostController.createPost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Atualiza um post existente pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: Post atualizado com sucesso
+ */
 router.put("/posts/:id", PostController.updatePost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Deleta um post pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do post
+ *     responses:
+ *       200:
+ *         description: Post deletado com sucesso
+ */
 router.delete("/posts/:id", PostController.deletePost);
 
 export default router;
